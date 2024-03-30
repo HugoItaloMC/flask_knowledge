aab5d3426a82991cd6cc3ba8523a73a22107626d
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2024-03-30 00:38:57 -0300

    FEAT (connection db): NEED CONNECTION TROUGHT HANDLERS AS FUNCTIONS

---
+++ flaskr/db.py
@@ -0,0 +1,41 @@
+import sqlite3
+import click
+
+# ` current_app `Objeto especial  que aponta uma manipulacão para aplicacão flask
+# `g` Outro objeto especial que é unico para cada requisicão
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row  # Conversa com a conexão para retornar linhas que agem como dicionários
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+
+    db = get_db()
+    with current_app.open_resource('schema.sql') as sql:
+        db.executescript(sql.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+    # Limpa o banco de dados se existr e criara novas tabelas
+    init_db()
+    click.echo('Iinitialize DataBase')
+
+
+def init_app(app):
+    # close_db e init_db_command são funcões necessárias para registros com a instância da aplicacão
+    app.teardown_appcontext(close_db)  # Chama o flask para limpar a subida apóes uma resposta
+    app.cli.add_command(init_db_command)  # Adiciona novos comandos que podem ser chamados a partir do comando `flask`
