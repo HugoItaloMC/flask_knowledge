ced7e521cc72baba992db78a7c681efad2f444f3
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2024-03-31 17:12:08 -0300

    FEAT (feature blog): ADDED FEATURE TO DO POSTS WITH AUTOR ID, UPDATE AND DELETE POST THAT WAS BE STORED TROUGHT VIEWS

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -15,7 +15,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 blue_auth = Blueprint('auth', __name__, url_prefix='/auth',  # Grupo de rotas associadas a rota '/auth'
-                      template_folder='template')  # Caminho do pacote com os templates
+                      template_folder='template')  # Caminho do pacote com os templatess
 
 
 @blue_auth.route('/register', methods=('GET', 'POST'))
---
+++ flaskr/blog.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
+
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp_blog = Blueprint('blog', __name__, template_folder='template')
+
+
+@bp_blog.route('/')
+def index():
+    #  Index vai mostrar vários os posts mais recentes, a consulta vai utilizar JOIN com ORDER BY
+
+    db = get_db()
+    posts = db.execute(
+        """
+        SELECT p.id, title, created, author_id, username FROM post p JOIN user u ON p.author_id = u.id
+        ORDER BY created DESC
+        """
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
