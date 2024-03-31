f45392c31fa9b474057d9ed5715c5236627a12e7
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2024-03-31 10:58:51 -0300

    FIX (redress TemplateNotFound): REDRESS THE TEMPLATE SEARCH IN APP

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -14,7 +14,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
-blue_auth = Blueprint('auth', __name__, url_prefix='/auth')  # Grupo de rotas associadas a rota '/auth'
+blue_auth = Blueprint('auth', __name__, url_prefix='/auth',  # Grupo de rotas associadas a rota '/auth'
+                      template_folder='template')  # Caminho do pacote com os templates
 
 
 @blue_auth.route('/register', methods=('GET', 'POST'))
@@ -44,7 +45,7 @@ def register():
                 return redirect(url_for("auth.login"))
 
         flash(error)
-    return render_template('register.html')
+    return render_template('auth/register.html')
 
 
 @blue_auth.route('/login', methods=('GET', 'POST'))
@@ -72,7 +73,7 @@ def login():
             return redirect(url_for('index'))
 
         flash(error)
-    return render_template('login.html')
+    return render_template('auth/login.html')
 
 
 #  Validando usuário se na sessão existe usuário logado
