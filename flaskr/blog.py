769c2df4ae1b9b3b42267b1a7f330cd157a32599
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2024-04-08 12:17:19 -0300

    FEAT (get posts): TAKE POSTS IN DATABASE WITH USER CURRENT

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -21,6 +21,7 @@ def index():
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
+
 @bp_blog.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -40,5 +41,24 @@ def create():
                 'INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)', (title, body, g.user['id'])
             )
             db.commit()
-            return redirect(url_for('blod.index'))
+            return redirect(url_for('blog.index'))
     return render_template('blog/create.html')
+
+
+def get_post(id_, check_author=True):
+    """
+
+    :param id_: Id do usuário logado
+    :param check_author: Condicão para verificar se o usuário logado é o dono do post a ser editado/deletado
+    :return: o conteúdo do post do usuário em questão
+    """
+    post = get_db().execute(
+        """SELECT p.id, title, body, created, author_id, username FROM post p JOIN user u ON p.author_id = u.id WHERE p.id = ?""", (id_,)
+    ).fetchone()
+    if post is None:
+        abort(404, "Post id %s doesn't exists" % id_)
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
