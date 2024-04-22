a14f3d4cc8e144b50b7ae13edf22571d592d30ab
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2024-04-22 20:30:08 -0300

    TEST (doc in func): DOCUMENTING  IN FUNCTIONS SHOULD THAT BE TO TESTS  IN VIEWS  'auth' ON  APPLICATION CURRENT

--- flaskr/tests/test_auth.py
+++ flaskr/tests/test_auth.py
@@ -4,6 +4,12 @@ from flaskr.db import get_db
 
 
 def test_register(client, app):
+    """
+
+    :param client: Fixeture that return an instance metod from Flask object be at  test_client() metod
+    :param app: Fixture from main instance this application current should as an Flask application instance
+    :return: assert this view to sign up new user at also and aassert query to database
+    """
     assert client.get('/auth/register').status_code == 200  # Requisicões GET devem retornar status code 200 para renderizar a página
 
     # Requisicões POST o usuário deve estar logado
@@ -23,5 +29,15 @@ def test_register(client, app):
                           ('a', '', b'Password is required'),
                           ('test', 'test', b'already redistered'),))
 def test_register_validate_input(client, username, password, message):
+    """
+
+    :param client: Fixeture 'client' in conftest file
+    :param username: Username send from Pytest decorator passed
+    :param password: Password send  from Pytest decorator passed
+    :param message: Message send from Pytest decorator passed
+    :return: assert response data
+    """
     response = client.post('/auth/register', data={'username': username, 'password': password})
     assert message in response.data
+
+
